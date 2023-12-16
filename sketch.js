3beaca626ace09b1d9cec72c25788191734a4273
lilyjung2 <153973221+lilyjung2@users.noreply.github.com>
2023-12-16 14:20:19 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+//let width=400, height=400;
+let width=windowwidth, height=windowheight;
+let stars=[];
+let factor=100;
+let speedSlider;
+
+function setup() {
+  createCanvas(width, height);
+  speedSlider=createSlider(0, 20, 5, 0.1);
+  for (let i=0; i<500; i++) {
+    stars[i]=createVector(
+      random(-width*factor, width*factor),
+      random(-height*factor, height*factor),
+      random(10,400));
+    stars[i].pz=stars[i].z;
+  }
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  noStroke();
+  translate(width/2, height/2);
+  for (let star of stars) {
+    let x=star.x/star.z;
+    let y=star.y/star.z;
+    let px=star.x/star.pz;
+    let py=star.y/star.pz;
+    let d=map(star.z, 0, 400, 10, 1);
+    circle(x, y, d);
+    //textSize(d*5);
+    //text("*", x, y);
+    stroke(255);
+    line(x, y, px, py);
+    star.pz=star.z;
+    star.z -= speedSlider.value();
+    if (star.z<1) {
+      star.x=random(-width*factor, width*factor);
+      star.y=random(-height*factor, height*factor);
+      star.z=400;
+      star.pz=400;
+    }
+  }
+}
